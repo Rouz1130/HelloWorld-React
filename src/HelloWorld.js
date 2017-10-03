2c22a9cfc1cbb54e7d72152e554e029e147312b3
rouzbeh.majlessi <rouzbeh.majlessi@avanade.com>
2017-10-03 12:48:59 -0700

    added HelloWorld component

---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+Import React, {Component} from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="helloContainer">
+      <h1>Hello, World</h1>
+    );
+  }
+}
+export default HelloWorld;
